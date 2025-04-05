4ad8399f41b23b2667469909462c9640f6d403ad
Eduardo Crespin <crespineduardo58@gmail.com>
2025-04-04 19:46:53 -0600

    Se sube archivos de landing-page

---
+++ includes/footer.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/stylees.css">
+    <!-- remix icon -->
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+    rel="stylesheet"/>
+    <title>Document</title>
+</head>
+<body>
+   <footer class="bg-dark">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row gy-5">
+                <!-- icons de redes sociales -->
+            <div class="col-lg-3 col-sm-6">
+                <a href="#"><h2 class="text-white">Kodigo Hotel</h2></a>
+                <div class="line"></div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, vel.</p>
+                <div class="social-icons">
+                    <a href="#"><i class="ri-facebook-fill"></i></a>
+                    <a href="#"><i class="ri-instagram-fill"></i></a>
+                    <a href="#"><i class="ri-youtube-fill"></i></a>
+                    <a href="#"><i class="ri-tiktok-fill"></i></a>
+                </div>
+            </div>
+            <!-- lista de otros servicios-footer -->
+            <div class="col-lg-3 col-sm-6">
+                <h5 class="mb-0 text-white">Otros Servicios</h5>
+                <div class="line"></div>
+             <ul class="footer-service">
+                <li><a href="#">Spa Rooms</a></li>
+                <li><a href="#">Room Service</a></li>
+                <li><a href="#">Pet Friendly</a></li>
+                <li><a href="#">A/C</a></li>
+             </ul>
+            </div>
+            <!-- lista de nosotros-footer -->
+            <div class="col-lg-3 col-sm-6">
+                <h5 class="mb-0 text-white">Nosotros</h5>
+                <div class="line"></div>
+             <ul class="footer-service">
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Precios</a></li>
+                <li><a href="#">Empresa</a></li>
+                <li><a href="#">Trabaja Con Nosotros</a></li>
+             </ul>
+            </div>
+            <!-- lista de contactos-footer -->
+            <div class="col-lg-3 col-sm-6">
+                <h5 class="mb-0 text-white">Contactos</h5>
+                <div class="line"></div>
+             <ul class="footer-service">
+                <li><a href="#">El Slavador - Panama</a></li>
+                <li><a href="#">7458-9563 / 2258-4525</a></li>
+                <li><a href="#">www.kodigoHotel.com</a></li>
+             </ul>
+            </div>
+            </div>
+        </div>
+        </div>
+        <!-- footer - derechos reservados -->
+        <div class="footer-bootom">
+            <div class="container">
+                <div class="row g-4 justify-content-between">
+                    <div class="col-auto">
+                        <p class="mb-0">Derechos Reservados &copy;. 2025</p>
+                    </div>
+                    <div class="col-auto">
+                        <p class="mb-0">Kodigo Hotel</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </footer>
+
+</body>
+</html>
