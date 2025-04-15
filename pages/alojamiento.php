e23912f8a91cf5a0869aeec70ba898f6f210c52c
Angélica Rodríguez <119092588+rodriguezac@users.noreply.github.com>
2025-04-15 14:40:31 -0500

    Update alojamiento.php

--- pages/alojamiento.php
+++ pages/alojamiento.php
@@ -86,8 +86,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
         aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" target="_blank">
-        <!-- agregar logo -->
-        <!-- <img src="" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo"> -->
         <span class="ms-1 font-weight-bold">Hotel Kodigo</span>
       </a>
     </div>
