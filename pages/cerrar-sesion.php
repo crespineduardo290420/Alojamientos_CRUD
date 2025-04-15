83ac6f3fc52334a7713df21aa08f2890d9f15241
Angélica Rodríguez <rodriguezq.angelicac@gmail.com>
2025-04-15 14:20:08 -0500

    Ajustes a comentarios de código

--- pages/cerrar-sesion.php
+++ pages/cerrar-sesion.php
@@ -1,4 +1,3 @@
-<!-- tiene que mandar afuera -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -96,7 +95,6 @@
   </script>
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="../assets/js/argon-dashboard.min.js?v=2.1.0"></script>
 </body>
 
--- pages/perfil.php
+++ pages/perfil.php
@@ -55,8 +55,6 @@ if ($result->num_rows > 0) {
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" target="_blank">
-        <!-- agregar logo -->
-        <!-- <img src="" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo"> -->
         <span class="ms-1 font-weight-bold">Hotel Kodigo</span>
       </a>
     </div>
